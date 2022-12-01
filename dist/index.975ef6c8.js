09d0623c1e1a544574ae72ff5cefbc18b08584d2
Aleksey <avralgroup@gmail.com>
2022-12-01 17:50:07 +0200

    processing

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -533,7 +533,34 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"8lqZg":[function(require,module,exports) {
 var _stylesCss = require("./css/styles.css");
+// import { fetchCountries } from './fetchCountries';
+// import debounce from 'lodash.debounce';
+// import { Notify } from 'notiflix';
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    inputEl: document.querySelector("search-box"),
+    listCountryEl: document.querySelector(".country-list"),
+    infoCountryEl: document.querySelector(".country-info")
+};
+fetch(`https://restcountries.com/v3.1/name/peru`).then((response)=>{
+    return response.json();
+}).then((name)=>{
+    console.log(name.car);
+// const markup = countryCard(name);
+// console.log(markup);
+}).catch((error)=>{
+    console.log(error);
+});
+function generationCountry(country) {
+    return `<li><img src="${country.flags}" alt="${country.official}" width="60" height="40">${country.official}</li>`;
+}
+console.log(generationCountry("country"));
+const generatingOneCountryInDom = (country)=>{
+    return `<h2><img src="" alt="${country.name}" width = 40px class="country-flag"> ${country.name}</h2>
+          <p><b>Capital:</b> ${country.capital}</p>
+          <p><b>Population:</b> ${country.population}</p>`;
+};
+console.log(generatingOneCountryInDom("country"));
 
 },{"./css/styles.css":"1CY4s"}],"1CY4s":[function() {},{}]},["ShInH","8lqZg"], "8lqZg", "parcelRequireabb0")
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix';
+// import { fetchCountries } from './fetchCountries';
+// import debounce from 'lodash.debounce';
+// import { Notify } from 'notiflix';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    inputEl: document.querySelector('search-box'),
+    listCountryEl: document.querySelector('.country-list'),
+    infoCountryEl: document.querySelector('.country-info'),
+};
+
+fetch(`https://restcountries.com/v3.1/name/peru`)
+    .then(response => {
+        return response.json();
+})
+.then(name => {
+    console.log(name.car);
+    // const markup = countryCard(name);
+    // console.log(markup);
+})
+.catch(error => {
+    console.log(error);
+});
+
+function generationCountry(country) {
+    return `<li><img src="${country.flags}" alt="${country.official}" width="60" height="40">${country.official}</li>`;
+}
+console.log(generationCountry('country'));
+
+const generatingOneCountryInDom = (country) => {
+  
+    return `<h2><img src="" alt="${country.name}" width = 40px class="country-flag"> ${country.name}</h2>
+          <p><b>Capital:</b> ${country.capital}</p>
+          <p><b>Population:</b> ${country.population}</p>`;
+};
+console.log(generatingOneCountryInDom('country'));
+
+
 
