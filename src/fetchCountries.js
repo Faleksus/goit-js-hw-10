7f8b2e0ffbd8eb39d67fdda8408d8036eb750382
Oleksiy Frolov <avralgroup@gmail.com>
2022-12-02 11:03:44 +0200

    corrected

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,25 @@
-const LINK_COUNTRIES = 'https://restcountries.com/v3.1/name/';
+const LINK_COUNTRIES = 'https://restcountries.com/v3.1/name';
 
-export function fetchCountries(name) {
-    return fetch(`${LINK_COUNTRIES}${name}?fields=name,capital,population,flags,languages`)
-      .then(response => {
+// export function fetchCountries(name) {
+//     return fetch(`${LINK_COUNTRIES}${name}?fields=name,capital,population,flags,languages`)
+//       .then(response => {
+//         return response.json();
+//       })
+//       .catch(error => {
+//         console.log(error);
+//     });
+//   };
+
+
+  const fetchCountries = name => {
+    return fetch(`${LINK_COUNTRIES}/${name}?fields=name,capital,population,flags,languages`).then(
+      response => {
+        if (response.status === 404) {
+          return Promise.reject(new Error());
+        }
         return response.json();
-      })
-      .catch(error => {
-        console.log(error);
-    });
+      },
+    );
   };
+  
+  export { fetchCountries };
